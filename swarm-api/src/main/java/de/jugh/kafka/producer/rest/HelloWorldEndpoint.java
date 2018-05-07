522c922565587c51e7d9c6970e47ba9b09cd079c
Matthias Wessendorf <matzew@apache.org>
2018-05-07 21:43:53 +0200

    Adding fake job

--- swarm-api/src/main/java/de/jugh/kafka/producer/rest/HelloWorldEndpoint.java
+++ swarm-api/src/main/java/de/jugh/kafka/producer/rest/HelloWorldEndpoint.java
@@ -5,6 +5,7 @@ import org.aerogear.kafka.cdi.annotation.KafkaConfig;
 import org.aerogear.kafka.cdi.annotation.Producer;
 
 import java.util.UUID;
+import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Path;
@@ -17,6 +18,8 @@ import javax.ws.rs.Produces;
 @KafkaConfig(bootstrapServers = "#{KAFKA_SERVICE_HOST}:#{KAFKA_SERVICE_PORT}")
 public class HelloWorldEndpoint {
 
+	private final static Logger logger = Logger.getLogger(HelloWorldEndpoint.class.getName());
+
 	@Producer
 	private SimpleKafkaProducer<String, String> myproducer;
 
@@ -27,11 +30,20 @@ public class HelloWorldEndpoint {
 	@Produces("text/plain")
 	public Response doGet() {
 
-		if (Math.random() > 0.2) {
-			myproducer.send(TOPIC_SUCCESS, "SUCCESS: " + UUID.randomUUID().toString());
-		} else {
-			myproducer.send(TOPIC_FAIL, "FAILED: " + UUID.randomUUID().toString());
-		}
+		// some fake...
+		final Job job = new Job();
+
+		logger.info("Job processing for " + job.getDeviceIDs().size() + " devices");
+
+
+		job.getDeviceIDs().forEach(deviceToken -> {
+
+			if (Math.random() > 0.2) {
+				myproducer.send(TOPIC_SUCCESS, job.getJobId(),"SUCCESS: " + deviceToken);
+			} else {
+				myproducer.send(TOPIC_FAIL,	job.getJobId(),"FAILED: " + deviceToken);
+			}
+		});
 
 		return Response.ok("Hello from WildFly Swarm!").build();
 	}
